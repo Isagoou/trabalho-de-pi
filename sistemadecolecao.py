7ae8ed3e145eced892e5c4bf3fac892eaaf11a40
Isagoou <93608965+Isagoou@users.noreply.github.com>
2022-02-16 22:01:21 -0300

    Add files via upload

---
+++ sistemadecolecao.py
@@ -0,0 +1,44 @@
+
+class sistema:
+    
+    def _init_(self, usuario):
+        qt.usuario=0
+        qt.itens=0
+        
+        
+    #Criação do sistema de cadastro do usuário
+    
+    def cadastroUsuario(self, nome, email, telefone, senha):
+        self.nome = nome
+        self.email= email
+        self.telefone = telefone
+        self.senha= senha
+        qt.usuario +=1
+        
+        
+    #Criação do Sistema de login:
+    
+    def login(self,usuario,senhadoUsuario):
+        usuario=nome
+        senhadoUsuario=senha
+        
+        if usuario!=nome or senha!=senhadoUsuario
+        print("Nome ou senha incorretos")
+        
+    #Cadastro de Item:
+    
+    def Item (self, nomeItem, valorOriginal, datadeProducao, valorPago, tipoItem):
+        self.nomeItem=nomeItem
+        self.valorOriginal=valorOriginal
+        self.datadeProducao=datadeProducao
+        self.valorPago=valorPago
+        self,tipoItem=tipoItem
+        qt.itens +=1
+    
+    #Excluir item:
+    def excluirItem:
+        def excluirItem(self, item)
+        del item
+        
+    
+        
